45339bf3d7079fe22a25b055a716fda059f9a0e5
billywestbury <billy.westbury@outlook.com>
2016-01-25 17:23:50 +0000

    blah blah blah

--- app/about/components/about.ts
+++ app/about/components/about.ts
@@ -1,44 +1,125 @@
 /// <reference path='../../../node_modules/phaser/typescript/phaser.comments.d.ts' />
 /// <reference path='../../../node_modules/phaser/typescript/pixi.comments.d.ts' />
 
-
-import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
-
-import {NameList} from '../../shared/services/name_list';
+import { Component } from 'angular2/core';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/common';
+import { NameList } from '../../shared/services/name_list';
 
 @Component({
-    selector: 'about',
-    templateUrl: './about/components/about.html',
-    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+  selector: 'about',
+  templateUrl: './about/components/about.html',
+  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class AboutCmp {
-    game: Phaser.Game;
+  game: Phaser.Game;
+  map: Phaser.Tilemap;
+  timeCheck: number = 0;
+  flipFlag: boolean = false;
+  startList: Array<number>;
+  squareList: Array<any> = [];
+  masterCounter: number = 0;
+  squareCounter: number = 0;
+  square1Num: any;
+  square2Num: any;
+  savedSquareX1: any;
+  savedSquareY1: any;
+  savedSquareX2: any;
+  savedSquareY2: any;
+  tileset: Phaser.Tileset;
+  layer: Phaser.TilemapLayer;
+  marker: any;
+  currentTile: any;
+  currentTilePosition: any;
+  tileBack: number = 25;
+  timesUp: any = '+';
+  youWin: any = '+';
+  myCountdownSeconds: any;
+  randomizeTiles: any;
+  tmpNumber: number = 0;
+  constructor() {
+    this.game = new Phaser.Game(600, 600, Phaser.AUTO, 'content2', {
+      preload: this.preload,
+      create: this.create,
+      update: this.update,
+      render: this.render
+    });
 
-    constructor() {
-        this.game = new Phaser.Game(584, 584, Phaser.AUTO, 'content2', {
-            preload: this.preload,
-            create: this.create,
-            update: this.update,
-            render: this.render
-        });
+  }
 
+  preload() {
+    console.log('preload: started');
+
+    this.game.load.tilemap('matching', './assets/phaser_tiles.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('tiles', './assets/phaser_tiles.png');//, 100, 100, -1, 1, 1);
+
+    console.log('preload: ended');
+  };
+
+  create() {
+    console.log('create: started');
+    this.map = this.game.add.tilemap('matching');
+    this.map.addTilesetImage('Desert', 'tiles');
+    //tileset = game.add.tileset('tiles');
+    this.layer = this.map.createLayer('Ground');//.tilemapLayer(0, 0, 600, 600, tileset, map, 0);
+    this.layer.resizeWorld();
+    this.marker = this.game.add.graphics(0, 0);
+    this.marker.lineStyle(2, 0x00FF00, 1);
+    this.marker.drawRect(0, 0, 100, 100);
+
+    // randomizeTiles();
+
+    for (var num = 1; num <= 18; num++) {
+      console.log(num);
+      // this.startList.push(num);
     }
+    // for (var num = 1; num <= 18; num++) {
+    //   this.startList.push(num);
+    // }
+    console.log(this.startList);
+    // for debugging
+    // myString1 = startList.toString();
+    //
+    // // randomize squareList
+    // for (i = 1; i <= 36; i++) {
+    //   let randomPosition = game.rnd.integerInRange(0, startList.length - 1);
+    //
+    //   let thisNumber = startList[randomPosition];
+    //
+    //   squareList.push(thisNumber);
+    //   let a = startList.indexOf(thisNumber);
+    //
+    //   startList.splice(a, 1);
+    // }
+    //
+    // // for debugging
+    // myString2 = squareList.toString();
+    //
+    // for (col = 0; col < 6; col++) {
+    //   for (row = 0; row < 6; row++) {
+    //     map.putTile(tileBack, col, row);
+    //   }
+    // }
 
-    preload() {
-        console.log('preload');
-    };
+    console.log('create: ended');
+  };
 
-    create() {
-        console.log('create');
-    };
+  update() {
+    // console.log('update');
+    // let vvv = this.countDownTimer();
+    // let timeLimit = 120;
+    // let mySeconds = this.game.time.totalElapsedSeconds();
+    // this.myCountdownSeconds = timeLimit - mySeconds;
+    // console.log(this.myCountdownSeconds);
+    // if (this.myCountdownSeconds <= 0) {
+    //   // time is up
+    //   this.timesUp = 'Time is up!';
+    // };
+  };
 
-    update() {
-        console.log('update');
-    };
+  render() {
+    // console.log('render');
+  };
 
-    render() {
-        console.log('render');
-    };
+
 
 }
--- app/home/components/home.ts
+++ app/home/components/home.ts
@@ -5,7 +5,7 @@ import {Component} from 'angular2/core';
 //import {InitTiles} from './utils';
 //import {updateTimer, createTimer, incrementScore, createTile, addTile, initTiles} from './func';
 
-function extractedFor(tileTypes:string[], random:Phaser.RandomDataGenerator){
+function extractedFor(tileTypes: string[], random: Phaser.RandomDataGenerator) {
   for (let x = 0; x < 8; x++) {
     for (let y = 0; y < 8; y++) {
       //Choose a random tile to add
@@ -29,128 +29,128 @@ function extractedFor(tileTypes:string[], random:Phaser.RandomDataGenerator){
 @Component({
   selector: 'home',
   templateUrl: './home/components/home.html',
-    styleUrls: ['./home/components/home.css']
+  styleUrls: ['./home/components/home.css']
 })
 
 export class HomeCmp {
-    game: Phaser.Game;
-    textValue: Phaser.Text;
-    backgroundSprite: any;
-    updateCount: number;
-    img: Phaser.Image;
+  game: Phaser.Game;
+  textValue: Phaser.Text;
+  backgroundSprite: any;
+  updateCount: number;
+  img: Phaser.Image;
 
-    startPosX: number;
-    startPosY: number;
-    activeTile1: any;
-    activeTile2: any;
-    canMove: boolean;
-    score: number;
-    tileWidth: any;
-    tileHeight: any;
-    tileGrid: Array<any>;
-    tiles: Phaser.Group;
-    random: any;
-    tile: Phaser.Sprite;
+  startPosX: number;
+  startPosY: number;
+  activeTile1: any;
+  activeTile2: any;
+  canMove: boolean;
+  score: number;
+  tileWidth: any;
+  tileHeight: any;
+  tileGrid: Array<any>;
+  tiles: Phaser.Group;
+  random: any;
+  tile: Phaser.Sprite;
 
-    constructor() {
+  constructor() {
 <<<<<<< Updated upstream
-        this.game = new Phaser.Game(584, 584, Phaser.AUTO, 'content1', {
+    this.game = new Phaser.Game(584, 584, Phaser.AUTO, 'content1', {
 =======
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
 >>>>>>> Stashed changes
-            create: this.create,
-            preload: this.preload,
-            update: this.update,
-            render: this.render
-        });
-    };
+      create: this.create,
+      preload: this.preload,
+      update: this.update,
+      render: this.render
+    });
+  };
 
-    preload() {
-        this.game.load.image('phaser-logo', './assets/phaser-logo.png');
+  preload() {
+    this.game.load.image('phaser-logo', './assets/phaser-logo.png');
 
-        // this.game.load.image('blue', './assets/gemBlue.png');
-        // this.game.load.image('green', './assets/gemGreen.png');
-        // this.game.load.image('red', './assets/gemRed.png');
-        // this.game.load.image('yellow', './assets/gemYellow.png');
-        this.game.load.image('tiles', './assets/matching.png');
-        this.game.load.tilemap('adv_time', 'assets/adv_time_map.json', null, Phaser.Tilemap.TILED_JSON);
-    };
+    // this.game.load.image('blue', './assets/gemBlue.png');
+    // this.game.load.image('green', './assets/gemGreen.png');
+    // this.game.load.image('red', './assets/gemRed.png');
+    // this.game.load.image('yellow', './assets/gemYellow.png');
+    this.game.load.image('tiles', './assets/matching.png');
+    this.game.load.tilemap('adv_time', './assets/adv_time_map.json', null, Phaser.Tilemap.TILED_JSON);
+  };
 
-    create() {
-        //this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.stage.backgroundColor = '34495f';
+  create() {
+    //this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.stage.backgroundColor = '34495f';
 
 <<<<<<< Updated upstream
-        let map = this.game.add.tilemap('adv_time');
+    let map = this.game.add.tilemap('adv_time');
 
-        //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
-        //  The second parameter maps this name to the Phaser.Cache key 'tiles'
-        map.addTilesetImage('adv_time_tileset', 'tiles', 146, 146, 1);
-    
-        //  Creates a layer from the World1 layer in the map data.
-        //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
-        let layer1 = map.createLayer('Tile Layer 1');
-      
+    //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
+    //  The second parameter maps this name to the Phaser.Cache key 'tiles'
+    map.addTilesetImage('adv_time_tileset', 'tiles', 146, 146, 1);
 
-        //  This resizes the game world to match the layer dimensions
-        layer1.resizeWorld();
-        // //Declare assets that will be used as tiles
-        // this.tileTypes = [
-        //     'blue',
-        //     'green',
-        //     'red',
-        //     'yellow'
-        // ];
- 
-        // //Keep track of the users score
-        // this.score = 0;
- 
-        // //Keep track of the tiles the user is trying to swap (if any)
-        // this.activeTile1 = null;
-        // this.activeTile2 = null;
- 
-        // //Controls whether the player can make a move or not
-        // this.canMove = false;
- 
-        // //Grab the weigh and height of the tiles (assumes same size for all tiles)
-        this.tileWidth = 146;
-        this.tileHeight = 146;
- 
-        // //This will hold all of the tile sprites
-        // this.tiles = this.game.add.group();
- 
-        // //Initialise tile grid, this array will hold the positions of the tiles
-        // //Create whatever shape you'd like
-        // this.tileGrid = [
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null],
-        //     [null, null, null, null, null, null, null, null]
-        // ];
-        // //Create a random data generator to use later
-        // let seed = Date.now();
-        // let random = new Phaser.RandomDataGenerator([seed]);
-        // let enemies = this.game.add.group();
+    //  Creates a layer from the World1 layer in the map data.
+    //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
+    let layer1 = map.createLayer('Tile Layer 1');
 
-        // for (let x = 0; x < 8; x++) {
-        //     //  This creates a new Phaser.Sprite instance within the group
-        //     //  It will be randomly placed within the world and use the 'baddie' image to display
-        //     //enemies.create((0.5 * i), 0.5, 'blue');
-        //     for (let y = 0; y < 8; y++) {
-        //         let tile = enemies.create(((x * this.tileWidth) + this.tileWidth / 2), (y *  this.tileHeight +( this.tileHeight / 2)), 'blue');
-        //         enemies.scale.setTo(0.5,0.5);
-        //         tile.anchor.setTo(0.5, 0.5);
-        //         //Keep a track of the tiles position in our tileGrid
-        // 		this.tileGrid[x][y] = tile;
-        //     }
-            
-        // }
- 
-// 01412320911 tracey
+
+    //  This resizes the game world to match the layer dimensions
+    layer1.resizeWorld();
+    // //Declare assets that will be used as tiles
+    // this.tileTypes = [
+    //     'blue',
+    //     'green',
+    //     'red',
+    //     'yellow'
+    // ];
+
+    // //Keep track of the users score
+    // this.score = 0;
+
+    // //Keep track of the tiles the user is trying to swap (if any)
+    // this.activeTile1 = null;
+    // this.activeTile2 = null;
+
+    // //Controls whether the player can make a move or not
+    // this.canMove = false;
+
+    // //Grab the weigh and height of the tiles (assumes same size for all tiles)
+    this.tileWidth = 146;
+    this.tileHeight = 146;
+
+    // //This will hold all of the tile sprites
+    // this.tiles = this.game.add.group();
+
+    // //Initialise tile grid, this array will hold the positions of the tiles
+    // //Create whatever shape you'd like
+    // this.tileGrid = [
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null],
+    //     [null, null, null, null, null, null, null, null]
+    // ];
+    // //Create a random data generator to use later
+    // let seed = Date.now();
+    // let random = new Phaser.RandomDataGenerator([seed]);
+    // let enemies = this.game.add.group();
+
+    // for (let x = 0; x < 8; x++) {
+    //     //  This creates a new Phaser.Sprite instance within the group
+    //     //  It will be randomly placed within the world and use the 'baddie' image to display
+    //     //enemies.create((0.5 * i), 0.5, 'blue');
+    //     for (let y = 0; y < 8; y++) {
+    //         let tile = enemies.create(((x * this.tileWidth) + this.tileWidth / 2), (y *  this.tileHeight +( this.tileHeight / 2)), 'blue');
+    //         enemies.scale.setTo(0.5,0.5);
+    //         tile.anchor.setTo(0.5, 0.5);
+    //         //Keep a track of the tiles position in our tileGrid
+    // 		this.tileGrid[x][y] = tile;
+    //     }
+
+    // }
+
+    // 01412320911 tracey
 =======
         //Declare assets that will be used as tiles
         let tileTypes = [
@@ -197,80 +197,80 @@ export class HomeCmp {
 
 
 >>>>>>> Stashed changes
-        // let style = { font: '36px Arial', fill: '#808080', align: 'center' };
-        // this.textValue = this.game.add.text(0, 0, '0', style);
-        // this.updateCount = 0;
-        // this.img = this.game.add.sprite(0, 0, 'phaser-logo');
-        // this.img.anchor.x = 0.5;
-        // this.img.anchor.y = 0.5;
+    // let style = { font: '36px Arial', fill: '#808080', align: 'center' };
+    // this.textValue = this.game.add.text(0, 0, '0', style);
+    // this.updateCount = 0;
+    // this.img = this.game.add.sprite(0, 0, 'phaser-logo');
+    // this.img.anchor.x = 0.5;
+    // this.img.anchor.y = 0.5;
 
 <<<<<<< Updated upstream
 
-        //  console.log(this.tileGrid);
+    //  console.log(this.tileGrid);
 =======
         console.log(this.tileGrid);
 >>>>>>> Stashed changes
-    };
+  };
 
 
-    update() {
-        // this.img.angle += 0.1;
-        // this.textValue.text = (this.updateCount++).toString();
-        //The user is currently dragging from a tile, so let's see if they have dragged
-        //over the top of an adjacent tile
-        if (this.activeTile1 && !this.activeTile2) {
+  update() {
+    // this.img.angle += 0.1;
+    // this.textValue.text = (this.updateCount++).toString();
+    //The user is currently dragging from a tile, so let's see if they have dragged
+    //over the top of an adjacent tile
+    if (this.activeTile1 && !this.activeTile2) {
 
-            //Get the location of where the pointer is currently
-            var hoverX = this.game.input.x;
-            var hoverY = this.game.input.y;
+      //Get the location of where the pointer is currently
+      var hoverX = this.game.input.x;
+      var hoverY = this.game.input.y;
 
-            //Figure out what position on the grid that translates to
-            var hoverPosX = Math.floor(hoverX / this.tileWidth);
-            var hoverPosY = Math.floor(hoverY / this.tileHeight);
+      //Figure out what position on the grid that translates to
+      var hoverPosX = Math.floor(hoverX / this.tileWidth);
+      var hoverPosY = Math.floor(hoverY / this.tileHeight);
 
-            //See if the user had dragged over to another position on the grid
-            var difX = (hoverPosX - this.startPosX);
-            var difY = (hoverPosY - this.startPosY);
+      //See if the user had dragged over to another position on the grid
+      var difX = (hoverPosX - this.startPosX);
+      var difY = (hoverPosY - this.startPosY);
 
-            //Make sure we are within the bounds of the grid
-            if (!(hoverPosY > this.tileGrid[0].length - 1 || hoverPosY < 0) && !(hoverPosX > this.tileGrid.length - 1 || hoverPosX < 0)) {
+      //Make sure we are within the bounds of the grid
+      if (!(hoverPosY > this.tileGrid[0].length - 1 || hoverPosY < 0) && !(hoverPosX > this.tileGrid.length - 1 || hoverPosX < 0)) {
 
-                //If the user has dragged an entire tiles width or height in the x or y direction
-                //trigger a tile swap
-                if ((Math.abs(difY) === 1 && difX === 0) || (Math.abs(difX) === 1 && difY === 0)) {
+        //If the user has dragged an entire tiles width or height in the x or y direction
+        //trigger a tile swap
+        if ((Math.abs(difY) === 1 && difX === 0) || (Math.abs(difX) === 1 && difY === 0)) {
 
-                    //Prevent the player from making more moves whilst checking is in progress
-                    this.canMove = false;
+          //Prevent the player from making more moves whilst checking is in progress
+          this.canMove = false;
 
-                    //Set the second active tile (the one where the user dragged to)
-                    this.activeTile2 = this.tileGrid[hoverPosX][hoverPosY];
+          //Set the second active tile (the one where the user dragged to)
+          this.activeTile2 = this.tileGrid[hoverPosX][hoverPosY];
 
-                    //Swap the two active tiles
-                    //   this.swapTiles();
+          //Swap the two active tiles
+          //   this.swapTiles();
 
-                    //After the swap has occurred, check the grid for any matches
-                    // this.game.time.events.add(500, function() {
-                    //     this.checkMatch();
-                    // });
-                }
-            }
+          //After the swap has occurred, check the grid for any matches
+          // this.game.time.events.add(500, function() {
+          //     this.checkMatch();
+          // });
         }
-    };
+      }
+    }
+  };
 
-    render() {
+  render() {
     //     this.game.debug.spriteInputInfo(this.tileGrid, 32, 32);
     // this.game.debug.geom(bunny.input._tempPoint);
-        // this.game.debug.text('This is drawn in render()', 0, 50);
-        // this.game.debug.text('Left Button: ' + this.game.input.activePointer.leftButton.isDown, 300, 132);
+    // this.game.debug.text('This is drawn in render()', 0, 50);
+    // this.game.debug.text('Left Button: ' + this.game.input.activePointer.leftButton.isDown, 300, 132);
 
-    };
+  };
 
-    tileDown(tile, pointer) {
-        //Keep track of where the user originally clicked
-        if (this.canMove) {
-            this.activeTile1 = tile;
-            this.startPosX = (tile.x - this.tileWidth / 2) / this.tileWidth;
-            this.startPosY = (tile.y - this.tileHeight / 2) / this.tileHeight;
-        }
+  tileDown(tile, pointer) {
+    //Keep track of where the user originally clicked
+    if (this.canMove) {
+      this.activeTile1 = tile;
+      this.startPosX = (tile.x - this.tileWidth / 2) / this.tileWidth;
+      this.startPosY = (tile.y - this.tileHeight / 2) / this.tileHeight;
     }
+  }
 };
