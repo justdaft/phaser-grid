77bbdec87635369a2a37d30867e7ba84f2c4d1aa
billywestbury <billy.westbury@outlook.com>
2016-01-25 14:06:38 +0000

    x

--- app/home/components/home.ts
+++ app/home/components/home.ts
@@ -5,9 +5,30 @@ import {Component} from 'angular2/core';
 //import {InitTiles} from './utils';
 //import {updateTimer, createTimer, incrementScore, createTile, addTile, initTiles} from './func';
 
+function extractedFor(tileTypes:string[], random:Phaser.RandomDataGenerator){
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      //Choose a random tile to add
+      let tileToAdd = tileTypes[random.integerInRange(0, tileTypes.length - 1)];
+      let tile = this.tiles.create(((x * this.tileWidth) + this.tileWidth / 2), (y * this.tileHeight + (this.tileHeight / 2)), tileToAdd);
+      this.tiles.scale.setTo(0.5, 0.5);
+      tile.anchor.setTo(0.5, 0.5);
+      //Enable input on the tile
+      tile.inputEnabled = true;
+
+      //  pick the right one accordingly.
+      tile.input.pixelPerfectOver = true;
+
+      //  Enable the hand cursor
+      tile.input.useHandCursor = true;
+      //Keep a track of the tiles position in our tileGrid
+      this.tileGrid[x][y] = tile;
+    }
+  }
+}
 @Component({
-    selector: 'home',
-    templateUrl: './home/components/home.html',
+  selector: 'home',
+  templateUrl: './home/components/home.html',
     styleUrls: ['./home/components/home.css']
 })
 
@@ -18,7 +39,8 @@ export class HomeCmp {
     updateCount: number;
     img: Phaser.Image;
 
-    tileTypes: Array<any>;
+    startPosX: number;
+    startPosY: number;
     activeTile1: any;
     activeTile2: any;
     canMove: boolean;
@@ -31,7 +53,11 @@ export class HomeCmp {
     tile: Phaser.Sprite;
 
     constructor() {
+<<<<<<< Updated upstream
         this.game = new Phaser.Game(584, 584, Phaser.AUTO, 'content1', {
+=======
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+>>>>>>> Stashed changes
             create: this.create,
             preload: this.preload,
             update: this.update,
@@ -54,6 +80,7 @@ export class HomeCmp {
         //this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.stage.backgroundColor = '34495f';
 
+<<<<<<< Updated upstream
         let map = this.game.add.tilemap('adv_time');
 
         //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
@@ -124,6 +151,52 @@ export class HomeCmp {
         // }
  
 // 01412320911 tracey
+=======
+        //Declare assets that will be used as tiles
+        let tileTypes = [
+            'blue',
+            'green',
+            'red',
+            'yellow'
+        ];
+
+        //Keep track of the users score
+        this.score = 0;
+
+        //Keep track of the tiles the user is trying to swap (if any)
+        this.activeTile1 = null;
+        this.activeTile2 = null;
+
+        //Controls whether the player can make a move or not
+        this.canMove = false;
+
+        //Grab the weigh and height of the tiles (assumes same size for all tiles)
+        this.tileWidth = 160;
+        this.tileHeight = 140;
+
+        //This will hold all of the tile sprites
+        this.tiles = this.game.add.group();
+
+        //Initialise tile grid, this array will hold the positions of the tiles
+        //Create whatever shape you'd like
+        this.tileGrid = [
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null]
+        ];
+        //Create a random data generator to use later
+        let seed = Date.now();
+        let random = new Phaser.RandomDataGenerator([seed]);
+        this.game.input.mouse.capture = true;
+      extractedFor.call(this, tileTypes, random);
+
+
+>>>>>>> Stashed changes
         // let style = { font: '36px Arial', fill: '#808080', align: 'center' };
         // this.textValue = this.game.add.text(0, 0, '0', style);
         // this.updateCount = 0;
@@ -131,35 +204,73 @@ export class HomeCmp {
         // this.img.anchor.x = 0.5;
         // this.img.anchor.y = 0.5;
 
+<<<<<<< Updated upstream
 
         //  console.log(this.tileGrid);
+=======
+        console.log(this.tileGrid);
+>>>>>>> Stashed changes
     };
 
 
     update() {
         // this.img.angle += 0.1;
         // this.textValue.text = (this.updateCount++).toString();
+        //The user is currently dragging from a tile, so let's see if they have dragged
+        //over the top of an adjacent tile
+        if (this.activeTile1 && !this.activeTile2) {
+
+            //Get the location of where the pointer is currently
+            var hoverX = this.game.input.x;
+            var hoverY = this.game.input.y;
+
+            //Figure out what position on the grid that translates to
+            var hoverPosX = Math.floor(hoverX / this.tileWidth);
+            var hoverPosY = Math.floor(hoverY / this.tileHeight);
+
+            //See if the user had dragged over to another position on the grid
+            var difX = (hoverPosX - this.startPosX);
+            var difY = (hoverPosY - this.startPosY);
+
+            //Make sure we are within the bounds of the grid
+            if (!(hoverPosY > this.tileGrid[0].length - 1 || hoverPosY < 0) && !(hoverPosX > this.tileGrid.length - 1 || hoverPosX < 0)) {
+
+                //If the user has dragged an entire tiles width or height in the x or y direction
+                //trigger a tile swap
+                if ((Math.abs(difY) === 1 && difX === 0) || (Math.abs(difX) === 1 && difY === 0)) {
+
+                    //Prevent the player from making more moves whilst checking is in progress
+                    this.canMove = false;
+
+                    //Set the second active tile (the one where the user dragged to)
+                    this.activeTile2 = this.tileGrid[hoverPosX][hoverPosY];
+
+                    //Swap the two active tiles
+                    //   this.swapTiles();
+
+                    //After the swap has occurred, check the grid for any matches
+                    // this.game.time.events.add(500, function() {
+                    //     this.checkMatch();
+                    // });
+                }
+            }
+        }
     };
 
     render() {
+    //     this.game.debug.spriteInputInfo(this.tileGrid, 32, 32);
+    // this.game.debug.geom(bunny.input._tempPoint);
         // this.game.debug.text('This is drawn in render()', 0, 50);
+        // this.game.debug.text('Left Button: ' + this.game.input.activePointer.leftButton.isDown, 300, 132);
+
     };
-    
 
-
-    // initTiles() {
-    //     for (var i = 0; i < 8; i++) {
-    //         for (var j = 0; j < 8; j++) {
-    //             var tileToAdd = 'blue';
-    //             let tile = this.tiles.create(0, 0, tileToAdd);
-    //             tile.anchor.setTo(0.5, 0.5);
-    //             tile.inputEnabled = true;
-    //             tile.tileType = tileToAdd;
-    //             // var tile = this.addTile(i, j);
-    //             this.tileGrid[i][j] = tile;
-                
-    //         }
-    //     }
-    //    console.log(this.tileGrid); 
-    // }
+    tileDown(tile, pointer) {
+        //Keep track of where the user originally clicked
+        if (this.canMove) {
+            this.activeTile1 = tile;
+            this.startPosX = (tile.x - this.tileWidth / 2) / this.tileWidth;
+            this.startPosY = (tile.y - this.tileHeight / 2) / this.tileHeight;
+        }
+    }
 };
